7da0b5b5239e72e57ca64e8b40909fd640b4c688
BOBLT <135662537+BOBLT@users.noreply.github.com>
2023-08-15 21:54:28 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var link = document.createElement('a');
   link.href = 'https://www.hawaiicommunityfoundation.org/maui-strong';
-  link.textContent = 'My Link';
+  link.textContent = 'HAWAII HAWAII HAWAII HAWAII HAWAII';
   
   var h1 = document.createElement('h1');
   h1.appendChild(link);
