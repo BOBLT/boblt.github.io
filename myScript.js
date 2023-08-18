3ecbc10041f573921031303ddddbc9c5dbfc0fea
BOBLT <135662537+BOBLT@users.noreply.github.com>
2023-08-17 20:09:04 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -9,8 +9,8 @@ window.onload = function() {
   document.body.appendChild(h1);
 };
 
-<script>
- <div id="clocks">
+
+<div id="clocks">
   <div id="pacific-time" class="clock">
     <div class="half-circle"></div>
     <div class="time-display">12:00</div>
@@ -20,32 +20,66 @@ window.onload = function() {
     <div class="time-display">12:00</div>
   </div>
 </div>
- #clocks {
-  display: flex;
-}
 
-.clock {
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
-  border-radius: 50%;
-  position: relative;
-}
+<style>
+  #clocks {
+    display: flex;
+  }
 
-.half-circle {
-  width: 50px;
-  height: 50px;
-  border-radius: 50px 50px 0 0;
-  background-color: lightblue;
-  position: absolute;
-  top: -1px;
-  left: calc(50% - 25px);
-}
+  .clock {
+    width: 100px;
+    height: 100px;
+    border: 1px solid black;
+    border-radius: 50%;
+    position: relative;
+  }
 
-.time-display {
-  position: absolute;
-  bottom: 10px;
-  width: 100%;
-  text-align: center;
-}
+  .half-circle {
+    width: 50px;
+    height: 50px;
+    border-radius: 50px 50px 0 0;
+    background-color: lightblue;
+    position: absolute;
+    top: -1px;
+    left: calc(50% - 25px);
+  }
+
+  .time-display {
+    position: absolute;
+    bottom: 10px;
+    width: 100%;
+    text-align: center;
+  }
+</style>
+
+<script>
+  const pacificTime = document.querySelector("#pacific-time .time-display");
+  
+  function setTime() {
+    const date = new Date();
+    
+    // Set time for Pacific Time
+    const pacificOffset = -7; // UTC offset for Pacific Time
+    date.setHours(date.getUTCHours() + pacificOffset);
+    
+    let hours = date.getHours();
+    
+    // Convert to military time
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    
+    let minutes = date.getMinutes();
+    
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    
+    pacificTime.textContent = `${hours}:${minutes}`;
+    
+    // Update every ten hours
+    setTimeout(setTime, 10 * 60 * 60 * 1000);
+  }
+  
+  setTime();
 </script>
